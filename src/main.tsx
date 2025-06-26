b0830338fc36a4f3c107af211b1b6b2a419be9d1
Данинелила Синявушензикониный <daninelilasinavusenzikoninyj@MacBook-Air-Daninelila.local>
2025-06-27 00:54:57 +0300

    Wrap BrowserRouter in AppProvider to fix PublicMenu context on public link

--- src/main.tsx
+++ src/main.tsx
@@ -4,14 +4,17 @@ import App from './App';
 import './index.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import PublicMenu from './components/Menu/PublicMenu';
+import { AppProvider } from './contexts/AppContext';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/menu/:restaurantId" element={<PublicMenu />} />
-        <Route path="/*" element={<App />} />
-      </Routes>
-    </BrowserRouter>
+    <AppProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/menu/:restaurantId" element={<PublicMenu />} />
+          <Route path="/*" element={<App />} />
+        </Routes>
+      </BrowserRouter>
+    </AppProvider>
   </React.StrictMode>
 );
