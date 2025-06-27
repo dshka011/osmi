de36be546d0db0fc5efdfb1efd5faddde979c6e3
Данинелила Синявушензикониный <daninelilasinavusenzikoninyj@MacBook-Air-Daninelila.local>
2025-06-27 15:14:47 +0300

    Исправить: публичное меню теперь всегда внутри всех провайдеров (LanguageProvider, AppProvider, NotificationProvider)

--- src/main.tsx
+++ src/main.tsx
@@ -4,14 +4,23 @@ import App from './App';
 import './index.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import PublicMenu from './components/Menu/PublicMenu';
+import { LanguageProvider } from './contexts/LanguageContext';
+import { AppProvider } from './contexts/AppContext';
+import { NotificationProvider } from './contexts/NotificationContext';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/menu/:restaurantId" element={<PublicMenu />} />
-        <Route path="/*" element={<App />} />
-      </Routes>
-    </BrowserRouter>
+    <NotificationProvider>
+      <LanguageProvider>
+        <AppProvider>
+          <BrowserRouter>
+            <Routes>
+              <Route path="/menu/:restaurantId" element={<PublicMenu />} />
+              <Route path="/*" element={<App />} />
+            </Routes>
+          </BrowserRouter>
+        </AppProvider>
+      </LanguageProvider>
+    </NotificationProvider>
   </React.StrictMode>
 );
